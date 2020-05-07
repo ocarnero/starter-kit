560fb19459daa9e002987fb956dad3c5a419c3d2
Oscar Carnero <Oscar.Carnero@endava.com>
2020-05-07 02:37:52 -0300

    unit testing with mocha and chai

---
+++ client/index.test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { JSDOM } from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+	it('should say hello', (done) => {
+		const dom = new JSDOM(fs.readFileSync('./client/index.html', "utf-8"));		
+		const h1 = dom.window.document.getElementsByTagName('h1')[0];
+		expect(h1.innerHTML).to.equal("Hello world");
+		done();
+		window.close();
+  });
+});
